705f72fa25f4d2a0410b70ca185281bcdeacc308
khis@stibodx.com <khis@stibodx.com>
2023-08-28 23:47:07 +0600

    add parcelable plugin and implement in Options class

--- permissionmanager/src/main/java/com/techascent/permissionmanager/Options.kt
+++ permissionmanager/src/main/java/com/techascent/permissionmanager/Options.kt
@@ -1,44 +1,17 @@
 package com.techascent.permissionmanager
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 
+@Parcelize
 data class Options(
 
     var settingsText: String? = "Settings",
     var rationaleDialogTitle: String? = "Permissions Required",
     var settingsDialogTitle: String? = "Permissions Required",
-    var settingsDialogMessage: String? = "Required permission(s) have been set"
-        .plus(" not to ask again! Please provide them from settings."),
+    var settingsDialogMessage: String? = "Requested permission(s) have been set"
+        .plus(" not to ask again. Please provide them from settings."),
     var sendBlockedToSettings: Boolean = true,
     var createNewTask: Boolean = false
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readByte() != 0.toByte()
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun writeToParcel(p0: Parcel?, p1: Int) {
-        TODO("Not yet implemented")
-    }
-
-    companion object CREATOR : Parcelable.Creator<Options> {
-        override fun createFromParcel(parcel: Parcel): Options {
-            return Options(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Options?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
